c204ea022d09cf0c5e8d76d972619edafaa2e575
AlessArver <alessarver@gmail.com>
2020-09-25 01:49:05 +0300

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import { Redirect, Route } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch, withRouter } from 'react-router-dom'
 
-import CartContainer from "./containers/Cart";
-import GoodsContainer from "./containers/Good";
+import { CartContainer, GoodContainer } from "./containers";
 import NavbarContainer from "./containers/Navbar";
 
 const App = () => {
   return (
     <div className="main__container">
       <NavbarContainer />
-      <Route exact path="/" render={() => <Redirect to="/goods" />} />
-      <Route path="/cart" render={() => <CartContainer />} />
-      <Route path="/goods" render={() => <GoodsContainer />} />
+       <Suspense fallback={<h1>Loading . . .</h1>}>
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/goods" />} />
+          <Route path="/cart" render={() => <CartContainer />} />
+          <Route path="/goods" render={() => <GoodsContainer />} />
+          <Route path='*' render={() => <div>404 Not Found</div>}/>
+        </Switch>
+      </Suspense>
     </div>
   );
 };
---
+++ src/containers/index.ts
@@ -0,0 +1,2 @@
+export const CartContainer = lazy(() => import('./Cart'))
+export const GoodContainer = lazy(() => import('./Good'))
