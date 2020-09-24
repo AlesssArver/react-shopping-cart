7679031c55243b0f1a67846a6aa44bd96d6eb6dd
AlessArver <alessarver@gmail.com>
2020-09-25 01:18:51 +0300

    Updates

---
+++ precache-manifest.8593fff7b443ba1436af056c432b8c49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e276bdc14c2f96b74868f886c86ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e631a9d69f7479e64e",
+    "url": "/static/css/2.1977312c.chunk.css"
+  },
+  {
+    "revision": "2b0f0f7a562f536f3f2d",
+    "url": "/static/css/main.645565d9.chunk.css"
+  },
+  {
+    "revision": "88e631a9d69f7479e64e",
+    "url": "/static/js/2.020d8533.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.020d8533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b0f0f7a562f536f3f2d",
+    "url": "/static/js/main.0c406dea.chunk.js"
+  },
+  {
+    "revision": "81d08e04a2e4215dd2ef",
+    "url": "/static/js/runtime-main.5856509f.js"
+  }
+]);
